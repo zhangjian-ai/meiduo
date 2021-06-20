cd5ea39f831e7e480d4b9043f6b0990e4a466de8
zhangjian <jian.zhang7@jiediankeji.com>
2021-06-21 00:58:07 +0800

    update new version

--- push.sh
+++ push.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "=============开始打包============="
-cd student-web
+cd meiduo_mall_frontend
 npm run build
 cd ..
 echo "=============打包完成============="
 echo "=============开始上传============="
-git add StudentV4BE student-web/dist student-web/Dockerfile student-web/nginx.conf Jenkinsfile
+git add .
 git commit -m "update new version"
-git push origin dev
+git push origin master
 echo "=============上传完成============="
 
 
